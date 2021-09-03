c57c86c0e73b6f5bd4e084638d9435367e1e37da
user <user@usernoMacBook-Pro.local>
2021-09-03 23:29:17 +0900

    Learn props of component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+// eslint-disable-next-line
+import { PLACEHOLDERS_FLIPPED_ALIAS } from "@babel/types";
+// eslint-disable-next-line 
+import { props } from "bluebird";
 import React from "react";
 
-/* function App() {
- return(
-  <React.Fragment>
-  <input type="text" onChange={() => {console.log("I am clicked!!")}} />;
-  </React.Fragment>
- )
-} */
 
 const App = () => {
+  const profiles = [
+    {name: "Taro",age:10},
+    {name: "Hanako",age:25},
+    {name:"noAge"},
+  ]
   return(
     <div>
-       <Cat />
-       <Cat />
-       <Cat />
-       <Cat />
+      {
+        profiles.map ((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!!!</div>
+const User = (props) => {
+  return <div>Hi!! I am {props.name},and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age : 1
 }
 
 export default App;

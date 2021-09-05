cb0eed0ae4640c4384e347addde7c7c17f85483f
user <user@usernoMacBook-Pro.local>
2021-09-05 14:38:15 +0900

    implement action and acrion creater

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+//アクションとは、Jsのオブジェクトのこと
+//typeはユニークなものでないといけない
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+export const decrement = ()=> ({
+    type: 'DECREMENT'
+})
